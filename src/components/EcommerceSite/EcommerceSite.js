971da495ab17562c67a18e1dc951516bbd686d00
v-nguyen6 <73795871+v-nguyen6@users.noreply.github.com>
2021-03-05 02:50:40 -0800

    etoile page site done

--- src/components/EcommerceSite/EcommerceSite.js
+++ src/components/EcommerceSite/EcommerceSite.js
@@ -2,23 +2,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
+import Flip from 'react-reveal/Flip';
 // Components
 import NavWorks from '../NavBar/NavWorks';
 import Footer from '../Footer/Footer';
 // Includes
-import headimg from '../../images/front-capstone.png';
+import headimg from '../../images/works-etoile.png';
+import portwire1 from '../../images/etoile-wireframes.png';
+import portcolor from '../../images/etoile-color-palette.png';
+//import portdev from '../../images/portfolio-code.png';
 
 function EcommerceSite(props) {
   return (
     <div>
       <NavWorks />
-      <div className="ecommerce">
+      <div className="portfolio" id="header">
 
-        <div className="ecommerce-header">
+        <div className="portfolio-header">
           <img src={headimg} alt="Etoile Header Image"/>
         </div>
 
-        <div className="ecommerce-content">
+        <div className="portfolio-content">
 
           <Fade right>
             <div className="scope">
@@ -33,71 +37,89 @@ function EcommerceSite(props) {
             </div>
           </Fade>
           
-          <div className="role-stack">
-            <div className="my-role">
-              <h3>My Role</h3>
-              <ul>
-                <li>Developer</li>
-                <li>UX/ UI Design</li>
-                <li>Mockup Design</li>
-                <li>Graphic Design</li>
-              </ul>
+          <Fade left>
+            <div className="role-stack">
+              <div className="my-role">
+                <h3>My Role</h3>
+                <ul>
+                  <li>Graphic Design in Development Process</li>
+                  <li>Information Architecture</li>
+                  <li>Development</li>
+                  <li>Content and Assets Management</li>
+                </ul>
+              </div>
+              <div className="dev-team">
+                <h3>Development Team</h3>
+                <ul>
+                  <li>Janice Lin</li>
+                  <li>Mina Bui</li>
+                  <li>Vinson Nguyen</li>
+                </ul>
+              </div>
+              <div className="work-tech-stack">
+                <h3>Tech Stack</h3>
+                <ul>
+                  <li>HTML/CSS</li>
+                  <li>JavaScript</li>
+                  <li>PHP</li>
+                  <li>SASS</li>
+                  <li>Figma</li>
+                  <li>Github</li>
+                  <li>Adobe Illustrator</li>
+                  <li>Adobe Photoshop</li>
+                </ul>
+              </div>
             </div>
-            <div className="dev-team">
-              <h3>Development Team</h3>
-              <ul>
-                <li>Mina Bui</li>
-                <li>Vinson Nguyen</li>
-              </ul>
+          </Fade>
+
+          <Fade right>
+            <div className="research-planning">
+              <h2>Research & Planning Process</h2>
+              <p>
+                The planning process was done linearly which helped in the efficiency and effectiveness while working in a team based collaborative setting. We started off by creating a detailed content plan followed by a site map and information architecture. Since the site would be developed using WordPress and Woocommerce, it was of utmost important to be able to identify where each page would utilise what PHP file. 
+              </p>
             </div>
-            <div className="work-tech-stack">
-              <h3>Tech Stack</h3>
-              <ul>
-                <li>HTML/CSS</li>
-                <li>React JavaScript Frameworks</li>
-                <li>The Movie Database API</li>
-                <li>Adobe Illustrator</li>
-                <li>Adobe Photoshop</li>
-              </ul>
+          </Fade>
+
+          <Flip left>
+            <div className="portfolio-wireframes">
+              <img src={portwire1} alt="Portfolio Wireframes - Home Page"/>
             </div>
-          </div>
+          </Flip>
 
-          <div className="research-planning">
-            <h2>Research & Planning Process</h2>
-            <p>
-              High fidelity mockups were produced months before the development phase of the project. Mockups were rendered fully through Photoshop to closely resemble how design work and web development may exist in a professional working environment, 
-              in which a web designer may be given a mockup through a Photoshop file and then is expected to follow the guidelines and created grids as closely as possible.  
-            </p>
-          </div>
+          <Fade left>
+            <div className="design-approach">
+              <h2>Design Approach</h2>
+              <p>
+                The main goal of Etoile was to create a brand that represented all the elements that each team member loved from a clothing brand and or store and melded it into one. This direction made Etoile focus on becoming a brand that was high-end yet casual with a hint of trendy street wear inspiration. This lead to us to designing a site that focussed on showcasing the products while maintaining a simple yet elegant and minimal layout. 
+              </p>
+              <h3>Color Palette</h3>
+              <img src={portcolor} alt="Portfolio Color Palette"/>
+            </div>
+          </Fade>
 
-          <div className="design-approach">
-            <h2>Design Approach</h2>
-            <p>
-              The Goal was to create a layout which was not only aesthetically pleasing but easy to read and view. We also wanted to incorporate as much details as possible to closely resemble the premade mockups and capture the vibe that it presented. 
-            </p>
-            <h3>Color Palette</h3>
-          </div>
+          <Fade right>
+            <div className="development">
+              <h2>Development</h2>
+              <p>
+                During the development of the site, it was important that the main structure of the website be done and completed first. We created an underscores theme, downloaded the proper plugins and moved onto to creating custom post types, taxonomies, and Advanced Custom Fields. Following the linear structure a base and functional site without styling was created first in order to make sure the functionality of the site was adequate before moving into the stylizing phase.
+              </p>
+            </div>
+          </Fade>
 
-          <div className="development">
-            <h2>Development</h2>
-            <p>
-              The project was developed using React JS while calling in data from an API from the Movie Database. The development process for the movie app was by far the most interesting as we utilized both functional components as well as class components when building the site. 
-              We were looking to challenge ourselves by trying to better our understanding of React by trying to demonstrate the past of React as well as the future. 
-            </p>
-          </div>
-          
-          <div className="challenges-takeaways">
-            <h2>Challenges & Takeaways</h2>
-            <p>
-              Although it was interesting that we were able to mesh 2 different ways and approaches to React, it overall proved challenging and at times cumbersome as it made for a more complicated structure. 
-              Overall this was a great learning process because we were able to develop the ability to understand which concept may be better during a specific part of development. 
-            </p>
-          </div>
+          <Fade right>
+            <div className="challenges-takeaways">
+              <h2>Challenges & Takeaways</h2>
+              <p>
+                COMING SOON.
+              </p>
+            </div>
+          </Fade>
         </div>
 
         <div className="site-btns">
           <Link className="site-btn-1" to={'/react'}>Previous Project</Link>
-          <Link className="site-btn-1" to={'/portfolio'}>Next Project</Link>
+          <Link className="site-btn-2" to={'/portfolio'}>Next Project</Link>
         </div>
 
       </div>
